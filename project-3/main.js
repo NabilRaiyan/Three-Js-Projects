933230481ef530c4dd5bc42820bafe3a6790ded9
NabilRaiyan <rraiyan77@gmail.com>
2024-06-18 20:26:22 +0600

    adding color and animation to the sphere

--- project-3/main.js
+++ project-3/main.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import "./style.css";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
+import gsap from "gsap";
 
 // scene 
 const scene = new THREE.Scene();
@@ -9,6 +10,7 @@ const scene = new THREE.Scene();
 const geomatry = new THREE.SphereGeometry(3, 70, 70);
 const material = new THREE.MeshStandardMaterial({
   color: "#00ff83",
+  roughness: 0.2,
   
 })
 
@@ -22,14 +24,15 @@ const sizes = {
 }
 
 // setting up the light
-const light = new THREE.PointLight(0xffffff, 150, 100);
+const light = new THREE.PointLight(0xffffff, 250, 100);
 light.position.set(0, 10, 10);
+
 scene.add(light);
 
 
 // creating a camera
 const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height, 0.1, 100);
-camera.position.z = 20;
+camera.position.z = 16;
 scene.add(camera);
 
 // rendering the scene into html
@@ -75,3 +78,31 @@ function animate(){
   controll.update();
 }
 animate();
+
+// Timeline magic (animating our content)
+const timeLine = gsap.timeline({defaults: {duration:1}});
+timeLine.fromTo(mesh.scale, {z:0, x:0, y:0}, {z:1, x:1, y:1});
+timeLine.fromTo('nav', {y: "-100%"}, {y: '0%'})
+timeLine.fromTo('.title', {opacity: 0}, {opacity: 1});
+
+// Mouse animation color
+let mouseDown = false;
+let rgb = [];
+window.addEventListener('mousedown', ()=>{
+  mouseDown = true
+});
+window.addEventListener('mouseup', ()=>{
+  mouseDown = false
+});
+
+window.addEventListener('mousemove', (e)=>{
+  if (mouseDown){
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255), Math.round((e.pageY / sizes.height) * 255), 150,
+    ]
+
+    // animate color
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
+    gsap.to(mesh.material.color, {r: newColor.r, g: newColor.g, b: newColor.b});
+  }
+})
