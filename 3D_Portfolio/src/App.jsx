1e1fee0d4a020e6e9d73d144f20c0dfc9bf1e1e5
NabilRaiyan <rraiyan77@gmail.com>
2024-10-20 17:50:01 +0600

    creating pages

--- 3D_Portfolio/src/App.jsx
+++ 3D_Portfolio/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
+import { Home, Contact, About, Projects } from "./pages"
 
 function App() {
 
@@ -9,10 +10,10 @@ function App() {
         <Router>
           <Navbar />
           <Routes>
-            <Route path="/" element={'Home'} />
-            <Route path="/about" element={'About'} />
-            <Route path="/projects" element={'Projects'} />
-            <Route path="/contact" element={'Contact'} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
 
         </Router>
